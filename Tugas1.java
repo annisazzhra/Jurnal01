1f0aee4440b62b48a34717e926ea39bfaf69f7f8
annisazzhra <nnisaazzahra730@gmail.com>
2023-09-28 21:55:27 +0700

    Push Jurnal 01

---
+++ Tugas1.java
@@ -0,0 +1,7 @@
+public class Tugas1 {
+    public static void main(String[] args){
+        System.out.println("Nama: Annisa Azzahra");
+        System.out.println("NIM: 6706223042");
+        System.out.println("Kelas: D3IF-46-04");
+    }
+}
